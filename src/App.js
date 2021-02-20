2f8d374fd0a1228809e124643f04dd6ddac21e22
Ismael <smartdom.biz@gmail.com>
2021-02-19 22:54:33 -0400

    Lo he logrado

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+ import "./styles/styles.scss"
+ import "./App.css"
+ import Logo from "./logo.svg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App=()=> 
+<>
+<h1>Hola mundo</h1>;
+<img src={Logo} className="App-logo" alt="logo"></img>
+
+</>
+
+
 
 export default App;
+
+
+//Reglas JSX
+// 1. Toda etiqueta debe cerrarse
+// 2. Los componentes deben devolver un sólo elemento padre
+// 3. Apoyarse de los fragmentos cuando necesito devolver dos elementos
+// 4. Fragment => <> hijos </> Importar {Fragment} from "react"
+// 5. Img siempre se cierra
+// 6. Las clases se asignan con className
+// 7. El atributo for del label pasa a ser htmlFor
